f51154d50675c56e63f2146a94a69e42b5934824
Jacob Martinson <jacobm3@gmail.com>
2022-11-28 22:11:55 -0600

    Consolidate scripts

--- add-vars-conn.sh
+++ add-vars-conn.sh
@@ -12,13 +12,23 @@ if [ -z "$AWS_DEFAULT_REGION" ]; then AWS_DEFAULT_REGION=us-east-2; fi
 
 SCHEDULER=$(docker ps | grep airflow | grep scheduler- | cut -f1 -d' ')
 
+echo
+echo "Adding AWS connection."
+echo
 docker exec -it -e PYTHONWARNINGS=ignore $SCHEDULER airflow connections add $CONN_NAME \
     --conn-json "{
         \"conn_type\": \"aws\",
-        \"login\": \"$AWS_ACCESS_KEY_ID\",
-        \"password\": \"$AWS_SECRET_ACCESS_KEY\",
-        \"extra\": {
-            \"aws_session_token\": \"$AWS_SESSION_TOKEN\",
-            \"region_name\": \"$AWS_DEFAULT_REGION\"
-        }
-    }"
+        \"login\": \"$AWS_ACCESS_KEY_ID\", \"password\": \"$AWS_SECRET_ACCESS_KEY\",
+        \"extra\": {\"aws_session_token\": \"$AWS_SESSION_TOKEN\", \"region_name\": \"$AWS_DEFAULT_REGION\" } }"
+
+# put sagemaker_role_name and bucket_id in env vars
+cd terraform
+eval $( terraform output | sed 's/ = /=/' )
+
+echo
+echo "Adding bucket and role variables."
+echo
+set -x
+docker exec -it -e PYTHONWARNINGS=ignore $SCHEDULER airflow variables set role $sagemaker_role_name
+docker exec -it -e PYTHONWARNINGS=ignore $SCHEDULER airflow variables set s3_bucket $bucket_id
+
